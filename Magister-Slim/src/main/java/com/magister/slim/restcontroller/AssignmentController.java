44418ff5a2dad9324fd0efb19a6a8a851f49c65e
naveenc <naveenc@INHYDNAVEENCH.COMAKEIT.COM>
2019-11-07 10:35:40 +0530

    new

--- Magister-Slim/src/main/java/com/magister/slim/restcontroller/AssignmentController.java
+++ Magister-Slim/src/main/java/com/magister/slim/restcontroller/AssignmentController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.magister.slim.entity.Assignment;
-import com.magister.slim.entity.StudyGuide;
-import com.magister.slim.entity.Theme;
-import com.magister.slim.entity.Unit;
 import com.magister.slim.references.StudyGuideReference;
 import com.magister.slim.references.ThemeReference;
 import com.magister.slim.references.UnitReference;
@@ -28,42 +25,38 @@ public class AssignmentController {
 	AssignmentAppService assignmentAppService;
 
 	Assignment assignment = new Assignment();
-	StudyGuide studyGuide=new StudyGuide();
-	Theme theme=new Theme();
-	Unit unit=new Unit();
 	StudyGuideReference studyGuideReference = new StudyGuideReference();
-	ThemeReference themeReference = new ThemeReference();
-	UnitReference unitReference = new UnitReference();
+	ThemeReference theme = new ThemeReference();
+	UnitReference unit = new UnitReference();
 
-	@RequestMapping(method = RequestMethod.POST)
-	public Assignment createAssignment(@RequestBody Assignment assignment) {
-		assignment.setActive(true);
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public Assignment add(@RequestBody Assignment assignment) {
 		Assignment status = assignmentAppService.addAssignment(assignment);
 		return status;
 	}
 
 	@RequestMapping(value = "/{assignmentId}", method = RequestMethod.DELETE)
-	public int deleteAssignmentDetails(@PathVariable("assignmentId") int assignmentId) {
+	public int delete(@PathVariable("assignmentId") int assignmentId) {
 		int status = assignmentAppService.deleteAssignment(assignmentId);
 		return status;
 	}
 
 	@RequestMapping(value = "/{assignmentId}", method = RequestMethod.PUT)
-	public Assignment updateAssignmentDetails(@PathVariable("assignmentId") int assignmentId, @RequestBody Assignment assignment) {
+	public Assignment update(@PathVariable("assignmentId") int assignmentId, @RequestBody Assignment assignment) {
 		assignment.setAssignmentId(assignmentId);
 		Assignment status = assignmentAppService.addAssignment(assignment);
 		return status;
 	}
 
-	@RequestMapping(value = "/{assignmentId}",method = RequestMethod.GET)
-	public Assignment getAssignmentDetail(@PathVariable("assignmentId") int assignmentId) {
+	@RequestMapping(method = RequestMethod.GET)
+	public Assignment get(@RequestParam int assignmentId) {
 		Assignment assignment = assignmentAppService.getAssignment(assignmentId);
 		return assignment;
 
 	}
 
 	@RequestMapping(value = "/{assignmentName}", method = RequestMethod.GET)
-	public List<Assignment> getAssignmentDetails(@RequestParam String assignmentName) {
+	public List<Assignment> get(@PathVariable("assignmentName") String assignmentName) {
 		List<Assignment> assignments = assignmentAppService.getAssignments(assignmentName);
 		return assignments;
 	}
--- Magister-Slim/src/main/java/com/magister/slim/util/JWTUtil.java
+++ Magister-Slim/src/main/java/com/magister/slim/util/JWTUtil.java
@@ -1,40 +1,32 @@
 package com.magister.slim.util;
 
 import java.sql.Date;
-
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier.BaseVerification;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.Clock;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
-import com.magister.slim.entity.User;
 
 public class JWTUtil {
 
 	private static final String JWT_SECRET = "My Secret";
-	
+
 	private static final String ISSUER = "Magister.slim";
 
 	private static final Algorithm JWT_ALGORITHM = Algorithm.HMAC256(JWT_SECRET);
 
-//	public static String generateToken(User payload) {
-//		String token = null;
-//		try {
-//			token = JWT.create().withIssuer(ISSUER).sign(JWT_ALGORITHM);
-//		} catch (JWTCreationException exception) {
-//		}
-//		return token;
-//	}
+	private static final long expiresAt = 5000L;
 
 	public static String generateToken(String payload) {
-		
+		Date now = new Date(System.currentTimeMillis() + expiresAt);
+		System.out.println(now);
+		System.out.println(now);
 		String token = null;
 		try {
-			token = JWT.create().withIssuer(ISSUER).withClaim("payload", payload).sign(JWT_ALGORITHM);
+			token = JWT.create().withIssuer(ISSUER).withExpiresAt(now).withClaim("payload", payload)
+					.sign(JWT_ALGORITHM);
 		} catch (JWTCreationException exception) {
 		}
 		return token;
@@ -42,48 +34,34 @@ public class JWTUtil {
 
 	public static boolean verifyToken(String token) {
 		try {
-			JWTVerifier verifier = JWT.require(JWT_ALGORITHM).withIssuer(ISSUER).acceptLeeway(1).acceptExpiresAt(5).build(); // Reusable verifier instance
+			JWTVerifier verifier = JWT.require(JWT_ALGORITHM).withIssuer(ISSUER).acceptLeeway(1).build(); 																						// instance
 			verifier.verify(token);
-//			BaseVerification verification = (BaseVerification) JWT.require(JWT_ALGORITHM)
-//				    .acceptLeeway(1)
-//				    .acceptExpiresAt(5);
-//				Clock clock = new Clock() {
-//					
-//					@Override
-//					public Date getToday() {
-//						return Date.valueOf("2019-11-03");
-//					}
-//				}; //Must implement Clock interface
-//				verifier = verification.build(clock);
 		} catch (JWTVerificationException exception) {
 			exception.printStackTrace();
 			System.out.println("Hii");
 			return false;
 		}
-		
 		return true;
-		
 	}
 
 	public static String getPayload(String token) {
 		DecodedJWT jwt = null;
-		String ss=null;
+		String ss = null;
 		try {
-		    jwt = JWT.decode(token);
-		    Date expiresAt = (Date) jwt.getExpiresAt();
-		    System.out.println(expiresAt);
-		    ss=jwt.getPayload();
-		} catch (JWTDecodeException exception){
+			jwt = JWT.decode(token);
+			ss = jwt.getPayload();
+			System.out.println(jwt.getExpiresAt());
+		} catch (JWTDecodeException exception) {
 		}
 		return ss;
 	}
-	
+
 	public static void main(String[] args) {
 		String payload = "MyPayload";
 		String token = generateToken(payload);
 		boolean valid = verifyToken(token);
 		String tokenPayload = getPayload(token);
-		
+
 		System.out.println(token);
 		System.out.println(valid);
 		System.out.println(tokenPayload);
