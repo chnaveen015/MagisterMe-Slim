689fe549c7e8465cefc6fab37451d6ff7c016700
naveenc <naveenc@INHYDNAVEENCH.COMAKEIT.COM>
2019-11-07 10:21:18 +0530

    small updataions

--- Magister-Slim/src/main/java/com/magister/slim/restcontroller/ThemeController.java
+++ Magister-Slim/src/main/java/com/magister/slim/restcontroller/ThemeController.java
@@ -1,62 +1,95 @@
 package com.magister.slim.restcontroller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.magister.slim.entity.StudyGuide;
 import com.magister.slim.entity.Theme;
+import com.magister.slim.entity.Unit;
 import com.magister.slim.references.StudyGuideReference;
+import com.magister.slim.references.UnitReference;
+import com.magister.slim.repository.StudyGuideInterface;
+import com.magister.slim.repository.ThemeInterface;
+import com.magister.slim.repository.UnitInterface;
 import com.magister.slim.service.ThemeAppService;
 
 @RestController
-@RequestMapping("studyguide/{studyGuideId}/themes")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("studyGuide/{studyGuideId}/theme")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class ThemeController {
 
 	@Autowired
+	StudyGuideInterface studyGuideInterface;
+	@Autowired
 	ThemeAppService themeAppService;
+	@Autowired
+	ThemeInterface themeInterface;
+	@Autowired
+	UnitInterface unitInterface;
 
-	@RequestMapping(value = "", method = RequestMethod.POST)
-	public Theme add(@RequestBody Theme theme, @PathVariable("studyGuideId") int studyGuideId) {
-		StudyGuideReference studyGuideReference = new StudyGuideReference();
+	StudyGuideReference studyGuideReference = new StudyGuideReference();
+	StudyGuide studyGuide = new StudyGuide();
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Theme createTheme(@RequestBody Theme theme, @PathVariable("studyGuideId") int studyGuideId) {
 		studyGuideReference.setStudyGuideId(studyGuideId);
-		theme.setStudyGuideReference(studyGuideReference);
-		Theme status = themeAppService.addTheme(theme);
-		System.out.println(status);
-		return status;
+		if (studyGuideInterface.findById(studyGuideId).isPresent()) {
+			studyGuide = studyGuideInterface.findById(studyGuideId).get();
+			studyGuideReference.setStudyGuideName(studyGuide.getStudyGuideName());
+			studyGuideReference.setActive(studyGuide.isActive());
+			theme.setStudyGuideReference(studyGuideReference);
+			Theme status = themeAppService.addTheme(theme, studyGuide);
+			System.out.println(status);
+			return status;
+		} else
+			return null;
 	}
 
 	@RequestMapping(value = "/{themeId}", method = RequestMethod.PUT)
-	public Theme update(@PathVariable("themeId") int themeId, @RequestBody Theme theme) {
-		theme.setThemeId(themeId);
-		Theme status = themeAppService.addTheme(theme);
-		System.out.println(status);
-		return status;
+	public Theme updateThemeDetails(@PathVariable("themeId") int themeId, @RequestBody Theme theme,
+			@PathVariable("studyGuideId") int studyGuideId) {
+		String themeName = theme.getThemeName();
+		theme = themeInterface.findById(themeId).get();
+		if (theme.getUnits() != null) {
+			List<UnitReference> unitReferences = theme.getUnits().stream().map(unitReference -> {
+				Unit unit = unitInterface.findById(unitReference.getUnitId()).get();
+				unit.getThemeReference().setThemeName(themeName);
+				unitInterface.save(unit);
+				return unitReference;
+			}).collect(Collectors.toList());
+			theme.setUnits(unitReferences);
+		}
+		studyGuide = studyGuideInterface.findById(studyGuideId).get();
+		studyGuide.getThemes().stream().filter((studyGuideReference) -> studyGuideReference.getThemeId() == themeId)
+				.findAny().get().setThemeName(themeName);
+		theme.setThemeName(themeName);
+		return themeAppService.updateTheme(theme, studyGuide);
 	}
 
 	@RequestMapping(value = "/{themeId}", method = RequestMethod.DELETE)
-	public int delete(@PathVariable("themeId") int themeId) {
-		int status = themeAppService.deleteTheme(themeId);
+	public int deleteThemeDetails(@PathVariable("themeId") int themeId,
+			@PathVariable("studyGuideId") int studyGuideId) {
+		int status = themeAppService.deleteTheme(themeId, studyGuideId);
 		return status;
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
-	public Theme get(@RequestParam int themeId) {
-		Theme theme = themeAppService.getTheme(themeId);
+	@RequestMapping(value = "/{themeId}", method = RequestMethod.GET)
+	public Theme getThemeDetail(@PathVariable("themeId") int themeId, @PathVariable("studyGuideId") int studyGuideId) {
+		Theme theme = themeAppService.getTheme(themeId, studyGuideId);
 		return theme;
 
 	}
 
-	@RequestMapping(value = "/{themeName}", method = RequestMethod.GET)
-	public List<Theme> get(@PathVariable("themeName") String themeName) {
-		List<Theme> themes = themeAppService.getThemes(themeName);
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Theme> getThemeDetails(@RequestParam String themeName, @PathVariable("studyGuideId") int studyGuideId) {
+		List<Theme> themes = themeAppService.getThemes(themeName, studyGuideId);
 		return themes;
-
 	}
 
 }
--- Magister-Slim/src/main/java/com/magister/slim/service/ThemeAppService.java
+++ Magister-Slim/src/main/java/com/magister/slim/service/ThemeAppService.java
@@ -1,51 +1,79 @@
 package com.magister.slim.service;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.magister.slim.entity.StudyGuide;
 import com.magister.slim.entity.Theme;
-import com.magister.slim.references.StudyGuideReference;
+import com.magister.slim.references.ThemeReference;
+import com.magister.slim.repository.StudyGuideInterface;
 import com.magister.slim.repository.ThemeInterface;
 
 @Service
 public class ThemeAppService {
-	
+
+	@Autowired
+	StudyGuideInterface studyGuideInterface;
 	@Autowired
 	ThemeInterface themeInterface;
-	
-	public List<Theme> getThemes(String themeName)
-	{
-		List<Theme> themes=themeInterface.getThemes(themeName);
-		return themes;
+	@Autowired
+	StudyGuideAppService studyGuideAppService;
+
+	public List<Theme> getThemes(String themeName, int studyGuideId) {
+		List<Theme> themes = themeInterface.getThemes(themeName);
+		List<Theme> themeReferences = themes.stream().map(themeReference -> {
+			if (themeReference.getStudyGuideReference().getStudyGuideId() == studyGuideId) {
+				return themeReference;
+			} else
+				return null;
+		}).collect(Collectors.toList());
+		return themeReferences;
 	}
-	public int deleteTheme(int themeId)
-	{
-		themeInterface.deleteById(themeId);
+
+	public int deleteTheme(int themeId, int studyGuideId) {
+		Theme theme = themeInterface.findById(themeId).get();
+		theme.setActive(false);
+		themeInterface.save(theme);
+		studyGuideAppService.deleteThemeReference(themeId, studyGuideId);
 		return themeId;
 	}
-	
-	public Theme addTheme(Theme theme,StudyGuideReference studyGuide)
-	{
-		theme.setActive(true);
-		theme.setThemeId(theme.getThemeId());
-		theme.setThemeName("Theme1");
-		theme.setUnits(theme.getUnits());
-		theme.setStudyGuideReference(studyGuide);
+
+	public Theme addTheme(Theme theme, StudyGuide studyGuide) {
 		themeInterface.save(theme);
+		studyGuide.setThemes(themeDetails(theme.getThemeId(), theme.getThemeName(), studyGuide));
+		studyGuideInterface.save(studyGuide);
 		return theme;
 	}
-	public Theme addTheme(Theme theme)
-	{
-		theme.setActive(true);
-		theme.setThemeId(theme.getThemeId());
-		theme.setThemeName("Theme1");
-		theme.setUnits(theme.getUnits());
+
+	public Theme updateTheme(Theme theme, StudyGuide studyGuide) {
 		themeInterface.save(theme);
+		studyGuideInterface.save(studyGuide);
 		return theme;
 	}
-	public Theme getTheme(int themeid) {
-		Theme theme=themeInterface.findById(themeid).get();
-		return theme;
+
+	public List<ThemeReference> themeDetails(int id, String themeName, StudyGuide studyGuide) {
+		ThemeReference themeReference = new ThemeReference();
+		List<ThemeReference> themes = new ArrayList<ThemeReference>();
+		themes = studyGuide.getThemes();
+		if (themes == null)
+			themes = new ArrayList<ThemeReference>();
+		themeReference.setThemeId(id);
+		themeReference.setThemeName(themeName);
+		themeReference.setActive(true);
+		themes.add(themeReference);
+		return themes;
+	}
+
+	public Theme getTheme(int themeid, int studyGuideId) {
+		if (themeInterface.findById(themeid).isPresent()) {
+			Theme theme = themeInterface.findById(themeid).get();
+			if (theme.getStudyGuideReference().getStudyGuideId() == studyGuideId)
+				return theme;
+			else
+				return null;
+		} else
+			return null;
 	}
 }
